59dc3d53aeba1471a2e69b6cf8793fd34b75ffbd
Nini <86172473+louxart@users.noreply.github.com>
2024-11-03 15:28:44 -0500

    feat: se implemento el inicio de sesion con reactive y registro

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,24 +1,64 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatSnackBarModule,
+    RouterLink,
+  ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-   email!: string;
-   password!:string;
+  loginForm: FormGroup;
+  private fb = inject(FormBuilder);
+  private snackBar = inject(MatSnackBar);
 
-   onSubmit(){
-    if(this.email && this.password)
-    {
-      console.log(this.email);
-      console.log(this.password);
-      console.log('Inicio de sesion exitoso');
+  constructor() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  controlHasError(control:string, error:string){
+      return this.loginForm.controls[control].hasError(error);
+  }
+
+  private showSnackBar(message: string) {
+    this.snackBar.open(message, 'Cerrar', {
+      duration: 3000,
+      verticalPosition: 'bottom',
+      horizontalPosition: 'center',
+    });
+  }
+
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const user = this.loginForm.value;
+
+      console.log('Usuario registrado:', user);
+      this.showSnackBar('Registro exitoso');
     }
-   }
+  }
 }
--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,12 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    RouterLink,
+    MatSelectModule
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
+  private fb = inject(FormBuilder);
+  private snackBar = inject(MatSnackBar);
+  private router= inject(Router);
 
+  constructor() {
+
+    this.registerForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      status: ['', [Validators.required]],
+      modalidad: ['', [Validators.required]]
+    });
+  }
+
+
+  controlHasError(control: string, error: string) {
+    return this.registerForm.controls[control].hasError(error) && this.registerForm.controls[control].touched;
+  }
+
+
+  private showSnackBar(message: string) {
+    this.snackBar.open(message, 'Cerrar', {
+      duration: 3000,
+      verticalPosition: 'bottom',
+      horizontalPosition: 'center',
+    });
+  }
+
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      const user = this.registerForm.value;
+
+      console.log('Usuario registrado:', user);
+      this.showSnackBar('Registro exitoso');
+      this.router.navigate(['/auth/login']);
+    }
+  }
 }
+
