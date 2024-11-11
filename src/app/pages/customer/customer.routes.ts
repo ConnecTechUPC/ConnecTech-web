107568534def05f51b3374b014ef53683613ccd2
Ximena Chavarria <maria.chavarria@utec.edu.pe>
2024-11-11 16:02:38 -0500

    feat: Se implemento el detalle de los eventos gratis y la inscripcion

--- src/app/pages/customer/customer.routes.ts
+++ src/app/pages/customer/customer.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CustomerLayoutComponent } from './customer-layaout/customer-layaout.component';
 import { EventCatalogComponent } from './event-catalog/event-catalog.component';
+import { EventDetailFreeComponent } from './event-detail-free/event-detail-free.component';
 import { EventDetailComponent } from './event-detail/event-detail.component';
 import { InscribeFormComponent } from './inscribe-form/inscribe-form.component';
 import { VotingComponent } from './voting/voting.component'; // Agrega esta ruta de votacion
@@ -11,6 +12,7 @@ export const customerRoutes: Routes = [
         component: CustomerLayoutComponent,
         children: [
             { path: 'catalog', component: EventCatalogComponent },
+            { path: 'catalog/:id', component: EventDetailFreeComponent },
             { path: 'catalog/:id', component: EventDetailComponent },
             { path: 'inscribe', component: InscribeFormComponent },
             { path: 'voting/:id', component: VotingComponent } // Ruta para la página de votación
