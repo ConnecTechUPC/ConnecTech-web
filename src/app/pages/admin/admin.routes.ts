cbc1cb824d8153d86b9f9dcd4e25f59d9c6252d1
Joaquin <joaquin.arevalo42@gmail.com>
2024-11-11 15:37:49 -0500

    Actualizacion del apartado Admin

--- src/app/pages/admin/admin.routes.ts
+++ src/app/pages/admin/admin.routes.ts
@@ -3,15 +3,25 @@ import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
 import { EventListComponent } from './event-list/event-list.component';
 import { EventFormComponent } from './event-form/event-form.component';
 
-export const adminRoutes: Routes = [
-    {
-        path:'',
-        component: AdminLayoutComponent,
-        children:[
-            {path: 'events', component: EventListComponent},
-            {path: 'events/add', component: EventFormComponent},
-            {path: 'events/edit/:id', component: EventFormComponent},
 
-        ]
-    }
-];
+
+export const adminRoutes: Routes = [
+        {
+          path: '',
+            component: AdminLayoutComponent, // El layout que engloba las rutas
+        children: [
+        {
+                path: 'events',
+                component: EventListComponent,
+              },
+              {
+                path: 'events/add',
+                component: EventFormComponent,
+              },
+              {
+                path: 'events/edit/:id',
+                component: EventFormComponent,
+              }
+            ]
+          }
+        ];
